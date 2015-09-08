2a1917c60157731e3ac78f3d51fa2b6ba8692c86
Tony <terimyaki@users.noreply.github.com>
2015-09-07 23:24:55 -0400

    renders d3 graphs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var childProcess = require('child_process'),
 
 var max = 0,
 	header,
-	points = [],
+	useData,
 	file = 'arduinoTest.txt',
 	fileStream = require('./fileStream')();
 
@@ -26,43 +26,137 @@ fileStream.on('data', function(data){
 		createSpecs(header);
 	}
 	else {
-		updateTable(data.Time, data.Voltage2_AR_Value, data.Voltage2_Calc, data.Resistance_Calc);
+		useData = updateData(data);
+		useData(updateTable);
+		useData(updateGraph);
 		if(data.Resistance_Calc > max) {
 			fileStream.pause();
 			max = data.Resistance_Calc;
-			d3.select('#max-container .resistance > p').text(max);
-			d3.select('#max-container .voltage > p').text(data.Voltage2_Calc);
+			updateMax(data.Voltage2_Calc, data.Resistance_Calc);
 			// fileStream.resume();
 			setTimeout(function(){
 				fileStream.resume();
 			}, 1000);
 		}
-		points.push(data);
 	}
 });
 
 fs.watchFile(file, function(){
 	fileStream.resume();
 });
+
 function createSpecs(specs){
-	var header = d3.select('#header');
-	Object.keys(specs).forEach(function(key){
-		var info = header.append('div'),
-			title = info.append('h5'),
-			value = info.append('p');
-		title.text(key);
-		value.text(specs[key]);
-	});
+	var data = Object.keys(specs),
+		header = d3.select('#header')
+					.selectAll('div')
+					.data(data)
+					.enter()
+					.append('div');
+
+	header.append('h5')
+		.text(function(d){return d;});
+
+	header.insert('p')
+		.text(function(d){
+			var info = specs[d];
+			if(isNaN(info)) return info;
+			else return d3.format(',d')(parseInt(info));
+		});
 }
 
-function updateTable(time, ar, voltage, resistance){
+function updateMax(voltage, resistance){
+	var formatVoltage = d3.format('.4f'),
+		formatResistance = d3.format(',.0f');
+
+	d3.select('#max-container .resistance > p')
+		.text(formatResistance(resistance));
+
+	d3.select('#max-container .voltage > p')
+		.text(formatVoltage(voltage));
+}
+
+function updateData(data){
+	if(!this.cache) this.cache = [];
+	this.cache.push(data);
+	return function(userFunc){
+		return userFunc.call(null, [].concat(this.cache));
+	};
+}
+
+function updateTable(data){
 	var table = d3.select('#readings table'),
-		row = table.append('tr');
-	[].slice.call(arguments).forEach(function(text){
-		row.append('td').text(text);
-	});
+		tableHeader = ['Time','Analog Reading','Voltage 2','Variable Resistance'],
+		formatTime = d3.time.format('%H:%M:%S'),
+		formatVoltage = d3.format('.4f'),
+		formatResistance = d3.format(',.0f'),
+		dataMatrix = data.map(function(d){
+			var time = formatTime(d.Time),
+				ar = d.Voltage2_AR_Value,
+				voltage = formatVoltage(d.Voltage2_Calc),
+				resistance = formatResistance(d.Resistance_Calc);
+			return [time, ar, voltage, resistance];
+		});
+
+	var tr = table.select('tbody')
+				.selectAll('tr')
+				.data(dataMatrix)
+				.enter()
+				.append('tr');
+
+	var theader = table.select('thead tr').empty() ? table.select('thead').append('tr').attr('class', 'mdl-data-table__cell--non-numeric') : table.select('thead tr'),
+		columns = theader.selectAll('th')
+				.data(tableHeader)
+				.enter()
+				.append('th')
+				.text(function(d){
+					return d;
+				});
+
+	var td = tr.selectAll('td')
+				.data(function(d) { return d; })
+				.enter().append('td')
+				.text(function(d) { return d; });
 }
 
-function createGraph(){
-	
+function updateGraph(data){
+	//svg size config
+	var margin = {
+			top : 20,
+			bottom : 20, 
+			left: 80,
+			right: 20
+		},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top -margin.bottom;
+
+	var x = d3.time.scale().range([0, width]),
+		y = d3.scale.linear().range([height, 0]),
+		xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(5),
+		yAxis = d3.svg.axis().scale(y).orient('left').ticks(5),
+		svg = d3.select('#graph').append('svg')
+								.attr('width', width + margin.left + margin.right)
+								.attr('height', height + margin.top + margin.bottom)
+								.append('g')
+								.attr('transform', 'translate(' + margin.left+ ',' + margin.top + ')'),
+		valueline = d3.svg.line().x(function(d) { return x(d.Time); }).y(function(d) { return y(d.Resistance_Calc); });
+		// Scale the range of the data
+		x.domain(d3.extent(data, function(d) { return d.Time; }));
+		y.domain([0, d3.max(data, function(d) { return d.Resistance_Calc; })]);
+
+		// Add the valueline path.
+		svg.append('path')
+			.attr('class', 'line')
+			.attr('d', valueline(data));
+
+		// Add the X Axis
+		svg.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0,' + height + ')')
+			.call(xAxis);
+
+		// Add the Y Axis
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
+
 }
